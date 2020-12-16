dae3b4ba9e8ccb298c47d56ff1b10c5bd28aea7d
NenadJovicic <n.jovicic357@gmail.com>
2020-12-16 20:40:16 +0100

    initialized project

---
+++ src/main/java/neon/release/project/NeonReleaseApplication.java
@@ -0,0 +1,13 @@
+package neon.release.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeonReleaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeonReleaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/neon/release/project/NeonReleaseApplicationTests.java
@@ -0,0 +1,14 @@
+package neon.release.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeonReleaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+        
+    }
+
+}
